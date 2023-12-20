443a1b493febe2a87330915420d177c4e9c7d3aa
Afidatunnisa <121389032+Afidatunnisa@users.noreply.github.com>
2023-12-20 21:34:05 +0700

    Delete firebase_config.js

--- firebase_config.js
+++
@@ -1,6 +0,0 @@
-const firebase = require ('firebase/app');
-const config = require ('./config');
-
-const firebase_config = firebase.initializeApp(config.firebaseConfig);
-
-module.exports = firebase_config;
